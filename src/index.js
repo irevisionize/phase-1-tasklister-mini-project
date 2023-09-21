8b66e13c737bf7173fe36eee126c4ebe4078e42f
Dominic <Dominic.decelle@gmail.com>
2023-09-21 15:01:05 -0700

    Completed Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,108 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// const { locationInfo } = require("jsdom/lib/jsdom/living/helpers/internal-constants")
+
+
+let globalColor = 'red'
+let highPriority = 'red'
+let mediumPriority = "orange"
+let lowPriority = "green"
+
+// const clickAnswer = document.addEventListener("DOMContentLoaded", () => {
+  let submitClick = document.querySelector('#create-task-form');
+  const selectPriority = document.querySelector('#priority')
+  selectPriority.addEventListener('change', (w) => {
+  const myValue = w.target.value
+  if (myValue === 'high'){
+    globalColor = highPriority
+  } else if (myValue === 'medium') {
+      globalColor = mediumPriority
+  } else { globalColor = lowPriority 
+    
+
+    } 
+    
+  })
+  submitClick.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTasks();
+  });
+// });
+
+
+
+
+
+
+
+function addTasks() {
+  let addingList = document.getElementById('new-task-description');
+  const newElement = document.createElement('div');
+  newElement.style.color = globalColor
+  const newBtn = document.createElement('button')
+  newBtn.textContent = 'x'
+  newBtn.addEventListener('click', () => {
+    newElement.remove()
+    newBtn.remove()
+  })
+  const inputValue = addingList.value;
+  newElement.textContent = inputValue;
+  const parentElement = document.querySelector('#tasks');
+  parentElement.append(newElement, newBtn);
+  
+  
+}
+
+
+
+
+
+
+// const addTasks = document.addEventListener(  () => {
+// const myTasks = document.querySelector('.tasks') 
+// myTasks.textContent = 
+
+// }); 
+
+
+
+
+
+// const taskDescription = document.addEventListener( () => {
+//   const newDescription = document.querySelector('#new-task-description')
+// newDescription.addEventListener('reset', () => {
+
+
+// })
+
+
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const need = document.querySelector('#submit')
+// const newDescription = document.querySelector('#new-task-description')
+// const submitClick = document.querySelector('#submit')
+// const submitClick = document.querySelector('#submit')
+
+
+
+// const clickAnswer = document.addEventListener("DOMContentLoaded", () => {
+//   let submitClick = document.querySelector('#submit')
+//   submitClick.addEventListener('click', (e) =>
+//   e.preventDefault()
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,68 @@
 // The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
 
-require("./helpers.js");
+// require("./helpers.js");
 
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
-    });
-  });
-});
+// describe("", () => {
+//   describe("", () => {
+//     it("Test passing", () => {
+//       return true;
+//     });
+//   });
+// });
+
+
+
+// document.addEventListener("DOMContentLoaded", () =>  {
+
+
+
+// }
+
+
+// //! CREATE GLOABAL VARIABLES 
+
+
+// const taskForm = document.querySelector("#create-task-form");
+// const taskUL = document.querySelector("#tasks")
+
+// //const 
+
+// function addTask(e) {
+//  //!block page refreshes 
+// e.preventDefault()
+//  //!Determine what user typed
+// const input = e.target["new-task-description"].value
+// //! Add new li to the collection with user input as text
+// const newLi = document.createElement('li)
+// newLi.textContent = input
+
+// const btn = document.createElement("button")
+// btn.addEventListener("click", () => newLi.remove() )
+// btn.textContent = "X"
+
+// newLi.appendChild(btn)
+// //!Append the li as child of ul
+// taskUL.appendChild(newLi)
+// //!Empty the form
+// e.target.reset()
+// }
+
+
+// const attachListeners = () => {
+// //! Attach all listeners we might need
+//   taskFormForm.addEventListener("submit", "addTask")
+
+// }
+
+
+// //Function Declaration
+// function attach() {
+//   //! When the user inserts todo allows you to 
+// }
+
+
+// //*have to call function for debugger
+// attachListeners();
+
+// //if i wanted to remove clear is Task.UL.innerText = ""
